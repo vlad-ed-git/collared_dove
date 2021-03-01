e02bc12d5d01dc03da7b036702fc0ad4f72c0f55
Vladimir Peter <vlad.edna@gmail.com>
2021-03-02 05:34:35 +0800

    added navigation graph

--- src/app/src/main/java/com/dev_vlad/collared_doves/MainActivity.kt
+++ src/app/src/main/java/com/dev_vlad/collared_doves/MainActivity.kt
@@ -2,10 +2,67 @@ package com.dev_vlad.collared_doves
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.dev_vlad.collared_doves.databinding.ActivityMainBinding
+import com.dev_vlad.collared_doves.utils.MyLogger
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private val TAG = MainActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_nav_host) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        //specify home fragments
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.poemsFragment,
+            )
+        )
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+        //hide and show menus depending on fragment
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+
+                R.id.poemsFragment -> {
+                    binding.apply{
+                        toolbar.isVisible = true
+                        toolbar.title = getString(R.string.poems_fragment_lbl)
+                    }
+                }
+
+                else -> {
+                    binding.toolbar.isVisible = true
+                }
+
+            }
+        }
+
     }
+
+
+    //toolbar handle back navigation
+    override fun onSupportNavigateUp(): Boolean {
+        MyLogger.logThis(TAG, location = "onSupportNavigateUp()", message =" -- called")
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
---
+++ src/app/src/main/java/com/dev_vlad/collared_doves/views/poems/PoemsFragment.kt
@@ -0,0 +1,10 @@
+package com.dev_vlad.collared_doves.views.poems
+
+import androidx.fragment.app.Fragment
+import com.dev_vlad.collared_doves.R
+
+class PoemsFragment : Fragment(R.layout.fragment_poems) {
+
+
+
+}
